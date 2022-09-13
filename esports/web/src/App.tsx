a4a4a82cb0018a22d0c9b8ac5c7c8de9bd038b9b
Vinicius Moura <viniciusrebelo487@gmail.com>
2022-09-12 22:08:19 -0300

    tailwindcss

--- esports/web/src/App.tsx
+++ esports/web/src/App.tsx
@@ -1,12 +1,89 @@
-
+import logo from './assets/Logologo-nlw.svg';
+import { MagnifyingGlassPlus } from 'phosphor-react';
 
 function App() {
-
   return (
-    <div className="App">
-      <h1>Is running</h1>
+    <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
+      <img src={logo} alt='Logo NLW' />
+      <h1 className='text-6xl text-white font-black mt-20'>
+        Seu{' '}
+        <span className='text-transparent bg-nlw-gradient bg-clip-text drop-shadow-md shadow-black'>
+          duo
+        </span>{' '}
+        está aqui.
+      </h1>
+
+      <div className='grid grid-cols-6 gap-6 mt-16'>
+        <a href='' className='relative rounded-lg overflow-hidden'>
+          <img src='/image 1.png' alt='' />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>
+              League of Legends
+            </strong>
+            <span className='text-zinc-300 text-sm block mt-1'>3 anuncios</span>
+          </div>
+        </a>
+        <a href='' className='relative rounded-lg'>
+          <img src='/image 2.png' alt='' />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Dota 2</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>3 anuncios</span>
+          </div>
+        </a>
+        <a href='' className='relative rounded-lg'>
+          <img src='/image 3.png' alt='' />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>CS:GO</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>3 anuncios</span>
+          </div>
+        </a>
+        <a href='' className='relative rounded-lg'>
+          <img src='/image 5.png' alt='' />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Apex Legends</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>3 anuncios</span>
+          </div>
+        </a>
+        <a href='' className='relative rounded-lg'>
+          <img src='/image 6.png' alt='' />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Fortinite</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>3 anuncios</span>
+          </div>
+        </a>
+        <a href='' className='relative rounded-lg'>
+          <img src='/image 7.png' alt='' />
+
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>WOW</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>3 anuncios</span>
+          </div>
+        </a>
+      </div>
+      <div className='mt-8 pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden'>
+        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+          <div>
+            {' '}
+            <strong className='text-2xl text-white font-black block'>
+              Não encontrou seu duo?
+            </strong>
+            <span className='text-zinc-400 block'>
+              Publique um anúncio para encontrar novos players!
+            </span>
+          </div>
+          <button className='py-4 px-3 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3'>
+            <MagnifyingGlassPlus size={24} />
+            Publicar Anúncio
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
